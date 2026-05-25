dce19620ea9de7af4e36b9a0018328cb284c4ef1
henrysolis35h <henrysolis35@gmail.com>
2026-05-24 20:55:51 -0500

    probando cccc

---
+++ Cadenas/caracteres_especiales_v1.py
@@ -0,0 +1,21 @@
+# Programa: Ejemplo de caracteres especiales en Python
+
+# Salto de linea \n
+mensaje1 = "Hola\nMundo"
+print(mensaje1)
+
+# Tabulacion \t
+mensaje2 = "\tJuan"
+print(mensaje2)
+
+# Comillas dentro de cadena
+mensaje3 = "Ella dijo: \"Hola a todos\" "
+print(mensaje3)
+
+# Barra invertida \
+mensaje4 = "C:\\Usuarios\\Marce\\Documentos"
+print(mensaje4)
+
+# Cadenas cruda (raw string) r
+mensaje5 = r"C:\Usuarios\Marce\Documentos"
+print(mensaje5)
